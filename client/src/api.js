273af71dd118349ea05850bd6ce83744a6cd4b7b
Abhinav Chaudhary <gamingdickens@gmail.com>
2026-08-31 14:43:25 +0530

    Backend connection

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const api = axios.create({ baseURL: "http://localhost:5000/api" });
+export const api = axios.create({ baseURL: `${import.meta.env.VITE_API_URL}/api` });
 api.interceptors.request.use((c) => {
   const t = localStorage.getItem("retro_token");
   if (t) c.headers.Authorization = `Bearer ${t}`;
